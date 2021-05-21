c92a91679910d01ea1d0cd7060047b555bed023d
lucas.maciel <lucasmacielgois@gmail.com>
2021-05-20 21:27:17 -0300

    Project created

---
+++ src/test/java/com/lmg/desafiojavaspringboot/AssemblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmg.desafiojavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
